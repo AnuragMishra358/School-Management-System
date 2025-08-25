ba849dd89abf2fb960028bfee320880f1a394403
Anurag Mishra CS1 18 <anuragmishra239914@gmail.com>
2025-08-25 22:56:11 +0530

    change baseApi

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
-// const baseApi = 'https://school-management-system-backend-m45s.onrender.com/api';
-const baseApi='http://localhost:5000/api'
+const baseApi = 'https://school-management-system-backend-m45s.onrender.com/api';
+// const baseApi='http://localhost:5000/api'
 
 export {baseApi}
